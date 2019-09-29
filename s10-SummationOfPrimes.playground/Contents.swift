3816111409bfa55f57697ded8a48880e24f438fa
yasinkbas <yasin.kbas12@gmail.com>
2019-09-29 16:20:32 +0300

    the filenames renamed

--- s10-SummationOfPrimes.playground/Contents.swift
+++ s10-SummationOfPrimes.playground/Contents.swift
@@ -3,7 +3,6 @@ import UIKit
 //The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 //
 //Find the sum of all the primes below two million.
-
 func sumPrimeNumbers(to: Int) -> Int {
     func isPrime(number:Int) -> Bool {
         let to = Int(sqrt(Double(number)) + 1)
